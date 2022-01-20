36f2c98cf23e02f0a6ac951c356c132b82fe2251
Milo <467127423@qq.com>
2022-01-20 15:06:43 +0800

    second commit

--- Web07_git/src/main/java/com/xb/User.java
+++ Web07_git/src/main/java/com/xb/User.java
@@ -3,4 +3,36 @@ package com.xb;
 public class User {
     private String name;
     private String adress;
+
+    public User() {
+    }
+
+    public User(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
 }
